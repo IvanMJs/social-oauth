19057f67683bb3f44ad4a636e51cf24a4f1ed579
Iván <ivanmeyer1991@gmail.com>
2021-08-26 13:20:32 -0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ButtonHTMLAttributes, useEffect, useRef } from "react";
+import { createClient } from "@supabase/supabase-js";
 
+const publicANONKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTk4MjE1OCwiZXhwIjoxOTQ1NTU4MTU4fQ.qSjDq7FvSLsQStU2uI-fxaClQCr1yStEpJz_h5xKlKg";
 function App() {
+  const supabaseClient = useRef<any>();
+  useEffect(() => {
+    supabaseClient.current = createClient(
+      "https://tzybddpxkidxkbpyonmv.supabase.co",
+      publicANONKey
+    );
+  });
+  const loginWithGithub = async (e:React.SyntheticEvent) => {
+    e.preventDefault();
+    const { user, session, error } = await supabaseClient.current.auth.signIn({
+      provider: "github",
+    });
+  };
+  const loginWithGoogle = async (e:React.SyntheticEvent) => {
+    e.preventDefault();
+    const { user, session, error } = await supabaseClient.current.auth.signIn({
+      provider: "twitch",
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <button onClick={loginWithGithub}>Login with Github</button>
+      </form>
+      <form>
+        <button onClick={loginWithGoogle}>Login with Twitch</button>
+      </form>
     </div>
   );
 }
-
 export default App;
